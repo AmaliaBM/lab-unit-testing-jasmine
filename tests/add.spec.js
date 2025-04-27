967ca57b43106edeeb6ee85c970a8bdb15a9a3c9
amalia barrigas <amaliabarrigas@gmail.com>
2025-04-27 12:13:52 +0200

    actividades terminadas sin bonus

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,17 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+    
+      //test 'it' escrito por mi
+
+      it("should return undefined if any of the two arguments is not a number", () => {
+        expect(add('', 2)).toEqual(undefined);
+        expect(add(2, '')).toEqual(undefined);
+        expect(add([], '')).toEqual(undefined);
+        expect(add({}, '')).toEqual(undefined);
+      });
+
+
     });
   });
   
